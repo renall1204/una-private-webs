12c2bf24932a4a1ab066d7e4328fffd42fb9e507
Renaldy Ari Gunawan <76225145+renall1204@users.noreply.github.com>
2025-01-13 23:06:37 +0700

    Update script.js

--- script.js
+++ script.js
@@ -3,15 +3,15 @@ const stories = {
     meeting: {
       title: "How We Met 💌",
       images: [
-        { src: "fm1.jpg", caption: "Pertama kali ketemu Rere (Face to Face) setelah jemput dia dari stasiun Gambir. Jujur gue deg2an abis takut dia ngeliat gw ga kaya ekspektasi dia. Ternyata pas ketemu malah dia yang lebih malu dong HAHA, i will never forget her genuine shyness and smile that night. Mana lupa isi e-money lagi pas pulang jadi minjem mobil belakang, untung koko-nya baik hati ya HAHAHA" },
-        { src: "fm2.jpg", caption: "Selama diperjalanan kita masih malu-malu kucing, and that night too is the very first time i hold her hand. It was so soft and i just couldn't took my palm off from her. We laugh at each other corny jokes but we surely felt love that night. And then i dropped her off in the Hotel Maya where she stays (luckily i got that Hotel on a steal deal too for the suite room!) " },
+        { src: "fm1.JPG", caption: "Pertama kali ketemu Rere (Face to Face) setelah jemput dia dari stasiun Gambir. Jujur gue deg2an abis takut dia ngeliat gw ga kaya ekspektasi dia. Ternyata pas ketemu malah dia yang lebih malu dong HAHA, i will never forget her genuine shyness and smile that night. Mana lupa isi e-money lagi pas pulang jadi minjem mobil belakang, untung koko-nya baik hati ya HAHAHA" },
+        { src: "fm2.JPG", caption: "Selama diperjalanan kita masih malu-malu kucing, and that night too is the very first time i hold her hand. It was so soft and i just couldn't took my palm off from her. We laugh at each other corny jokes but we surely felt love that night. And then i dropped her off in the Hotel Maya where she stays (luckily i got that Hotel on a steal deal too for the suite room!) " },
       ],
     },
     first_date: {
       title: "Our First Date 🌸",
       images: [
         { src: "fm3.jpg", caption: "Well technically our first date was 'Kondangan Date' but it's never really a date isn't it? So yeah the next day we to IRUUM which is a korean restaurant that got viral recently. We check this one out and it has a lot of activities to do, one of them is that we took a photo studio with korean uniform theme and the result is so damn good!" },
-        { src: "fm4.jpg", caption: "This is the result, she looks so cute right? on the other hand i look like one of those back-seaters boys who just go sleeping all day long. Sejujurnya gue lebih mirip kaya anak STM yang pacaran sama anak swasta ya gak sih? wkwkwkwk" },
+        { src: "fm4.JPG", caption: "This is the result, she looks so cute right? on the other hand i look like one of those back-seaters boys who just go sleeping all day long. Sejujurnya gue lebih mirip kaya anak STM yang pacaran sama anak swasta ya gak sih? wkwkwkwk" },
       ],
     },
     milestones: {
@@ -19,7 +19,7 @@ const stories = {
       images: [
         { src: "aq4.jpg", caption: "Our first achievement kayanya pas di Jakarta Aquarium sih. Jujur gue kaget ni cewe kok berani banget woy dipeluk uler piton gitu? i mean just look at the photo lol gua yang fotoin aja sampe merinding tapi ni cewe ngga? heran sih wkwkwkwk gak sengaja juga malah ketemu temen kampusnya dia disini, Rere seems so happy :)" },
         { src: "rt5.jpg", caption: "Our first Road Trip! Jujur ini juga road trip TERPANJANG pertama kalinya buat gue, ini bayangin aja Cilacap - Bandung yang jaraknya 300 kilo lebih dan gue bawa mobil dia sendiri HAHA tapi hebatnya si Rere menolak untuk tidur dan malah nemenin gue selama perjalanan. Thank you ya walaupun sebenernya tidur juga gapapa sih biar lagunya bisa diganti non K-Pop HAHAHA JK i love Park Shin Hye :)" },
-        { src: "rt3.jpg", caption: "Our First Concert soon?" },
+        { src: "rt3.JPG", caption: "Our First Concert soon?" },
     ],
     },
   };
